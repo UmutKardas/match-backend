de778c5f031e3d7cadbf2a00a359b96641615efb
Hüseyin Umut Kardaş <kardasumt@gmail.com>
2026-01-26 14:26:29 +0300

    Add Mongoose schemas to match and user modules
    Integrated MongooseModule with Match and User modules by registering their respective schemas. This enables MongoDB schema support for both modules.

--- src/modules/matches/match.module.ts
+++ src/modules/matches/match.module.ts
@@ -1,8 +1,10 @@
 import { Module } from "@nestjs/common";
 import { MatchService } from "./match.service";
+import { MongooseModule } from "@nestjs/mongoose";
+import { Match, MatchSchema } from "./match.schema";
 
 @Module({
-    imports: [],
+    imports: [MongooseModule.forFeature([{ name: Match.name, schema: MatchSchema }])],
     providers: [MatchService],
     exports: []
 })
--- src/modules/users/user.module.ts
+++ src/modules/users/user.module.ts
@@ -1,9 +1,11 @@
 import { Module } from "@nestjs/common";
 import { UserService } from "./user.service";
 import { UserSeed } from "./user.seed";
+import { MongooseModule } from "@nestjs/mongoose";
+import { User, UserSchema } from "./user.schema";
 
 @Module({
-    imports: [],
+    imports: [MongooseModule.forFeature([{ name: User.name, schema: UserSchema }])],
     providers: [UserService, UserSeed],
     exports: []
 })
