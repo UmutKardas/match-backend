4621726e1a9c951b8d9eaa8d89a17711c6df96c8
Hüseyin Umut Kardaş <kardasumt@gmail.com>
2026-01-27 22:19:49 +0300

    Validate leaderboard mode in controller
    Added a check to ensure the 'mode' query parameter is a valid LeaderboardMode. Throws an AppException if the mode is invalid to prevent improper requests.

--- src/modules/leaderboard/leaderboard.controller.ts
+++ src/modules/leaderboard/leaderboard.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Get, Query } from "@nestjs/common";
 import { LeaderboardMode } from "src/common/constants/leaderboard.constants";
 import { LeaderboardService } from "./leaderboard.service";
 import { LeaderboardResponseDto } from "./dto/leaderboard-response.dto";
+import { AppException } from "src/common/exceptions/app.exception";
 
 @Controller('leaderboard')
 export class LeaderboardController {
@@ -11,7 +12,10 @@ export class LeaderboardController {
 
     @Get()
     async getLeaderboard(@Query('userId') userId: string, @Query('mode') mode: LeaderboardMode): Promise<LeaderboardResponseDto> {
-        const data = this.leaderboardService.getLeaderboard(userId, mode);
-        return data;
+        if (!Object.values(LeaderboardMode).includes(mode as LeaderboardMode)) {
+            throw new AppException(`Invalid mode: ${mode}`);
+        }
+
+        return await this.leaderboardService.getLeaderboard(userId, mode);
     }
 }
